9a291c882f8d531eaefaa9ead261799ee241be68
david <cdavidlopez@hotmail.com>
2019-09-22 17:48:01 -0500

    prueba juego html

---
+++ src/main/resources/static/js/inicio.js
@@ -0,0 +1,30 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    inicio.iniciarJuego();
+  },
+  false
+);
+
+var inicio = {
+  iniciarJuego: function() {
+    console.log("Juego iniciado");
+    teclado.iniciar();
+    dimiensiones.iniciar();
+    inicio.recargarTiles();
+    bublePrincipal.iterar();
+  },
+  recargarTiles: function() {
+    document.getElementById("juego").innerHTML = "";
+    for (var y = 0; y < dimiensiones.obtenerTilesVerticales(); y++) {
+      for (var x = 0; x < dimiensiones.obtenerTilesHorizontales(); x++) {
+        var r = new Rectangulo(
+          x * +dimiensiones.ladoTildes,
+          y * dimiensiones.ladoTildes,
+          dimiensiones.ladoTildes,
+          dimiensiones.ladoTildes
+        );
+      }
+    }
+  }
+};

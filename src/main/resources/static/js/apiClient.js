3ffd1148f2b7f1748a5c09a9f18581644564dd9f
unknown <2133561@labinfo.is.escuelaing.edu.co>
2019-10-02 07:54:48 -0500

    apiclient

--- src/main/resources/static/js/apiClient.js
+++ src/main/resources/static/js/apiClient.js
@@ -1,6 +1,24 @@
-const apiUri="";
+const apiUri="capturetheflag-arsw.herokuapp.com/";
 apliClient= (function(){
     return {
-        
+
+        saveCuenta: function(cuenta){
+            $.ajax({
+                url: "cuentas/" + cuenta ,
+                type: "POST",
+                data: newBp,
+                contentType: "application/json"
+            });
+        },
+
+        checkPassword: function(nick,password, callback){
+            jQuery.ajax({
+                url: apiUrl + "cuentas/" + nick ,
+                success: function(result) {
+                  callback(result);
+                },
+                async: true
+            });
+        }
     }
 })

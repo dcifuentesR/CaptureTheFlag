be5a5e5528df565177434f4be15c31d2c134ad7b
diego2097 <ye.yo14@hotmail.com>
2019-10-14 20:48:09 -0500

    cookies de sesion

--- src/main/java/edu/eci/arsw/CaptureTheFlag/CaptureTheFlagApplication.java
+++ src/main/java/edu/eci/arsw/CaptureTheFlag/CaptureTheFlagApplication.java
@@ -9,5 +9,4 @@ public class CaptureTheFlagApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CaptureTheFlagApplication.class, args);
 	}
-
 } 	
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -18,6 +18,7 @@ var appModule = (function(){
         var password = $('#password').val();
         if (cuenta.contrasena === password){
             location.href = "/home.html"
+            verificationModule.crear_cookie_sesion($('#nick').val())
         }
         else {
             alert("Incorrect password");
@@ -29,3 +30,42 @@ var appModule = (function(){
         addAcount: addAcount
     }
 })();
+
+var verificationModule =(function(){
+
+    var crear_cookie_sesion =  function(usuario){
+        document.cookie = "nickname=" +  encodeURIComponent(usuario);
+    }
+
+    var _readCookie = function (name) {
+        var nameEQ = name + "="; 
+        var ca = document.cookie.split(';');
+      
+        for(var i=0;i < ca.length;i++) {
+      
+          var c = ca[i];
+          while (c.charAt(0)==' ') c = c.substring(1,c.length);
+          if (c.indexOf(nameEQ) == 0) {
+            return decodeURIComponent( c.substring(nameEQ.length,c.length) );
+          }
+      
+        }
+        return null;
+      }
+
+
+    var check_cookie = function(){
+        /*var user = _readCookie("nickname");
+        if (user == null ){
+            alert("Permiso denegado, debe logearse primero.")
+            location.href = "/index.html"
+        }*/
+    }
+
+    return {
+        crear_cookie_sesion: crear_cookie_sesion,
+        check_cookie: check_cookie
+    }
+
+})(); 
+
